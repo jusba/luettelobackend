a418011176996db2aa41be8c4440e9e37dbaaa57
jusba <jviljami@gmail.com>
2020-02-06 19:59:55 +0200

    .gitignore ja mongoose

---
+++ mongo.js
@@ -0,0 +1,42 @@
+/*mongodb+srv://jusba:<password>@cluster0-2encc.mongodb.net/test?retryWrites=true&w=majority*/
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const nameinput = process.argv[3]
+const numberinput = process.argv[4]
+
+const url = `mongodb+srv://jusba:${password}@cluster0-2encc.mongodb.net/people?retryWrites=true&w=majority`
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(password)
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+    
+  })
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: nameinput,
+    number: numberinput
+  })
+  
+if (nameinput === undefined || numberinput === undefined){
+    console.log("Phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(
+            person.name,person.number
+          
+          )
+        })
+        mongoose.connection.close()
+      })
+}else {
+  person.save().then(response => {
+    console.log('Added',person.name, "number", person.number,"to phonebook");
+    mongoose.connection.close();
+  })
+
+}
+
+ 
