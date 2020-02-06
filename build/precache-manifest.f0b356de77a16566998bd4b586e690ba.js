191091f4b11144b3805e65a98de871a036cd7c16
jusba <jviljami@gmail.com>
2020-02-06 17:53:15 +0200

    add build

---
+++ build/precache-manifest.f0b356de77a16566998bd4b586e690ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1508ea67f66eca5c8e61f717fd717287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2822e0e8e7b9ffe82fa",
+    "url": "/static/css/main.3ca9e502.chunk.css"
+  },
+  {
+    "revision": "4b18cf36501447135d88",
+    "url": "/static/js/2.7c9eebd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c9eebd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2822e0e8e7b9ffe82fa",
+    "url": "/static/js/main.d1ea74a8.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
